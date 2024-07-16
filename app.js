e29390ac171d9d4b4250a294f56f8eb980a90869
Salc-WM <150378169+Salc-wm@users.noreply.github.com>
2024-07-16 00:33:07 -0400

    Update app.js
    Print all services

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 #!/usr/bin/env node
-import fetchStatus from './rockstarGames-status.js'
-import { readFile, writeFile } from 'fs'
+import { fetchStatus } from './rockstarGames-status.js'
 
-function jsonReader(filePath, cb) {
-    readFile(filePath, (err, fileData) => {
-        if (err) {
-             return cb && cb(err)
+fetchStatus().then((statuses) => {
+    console.log('All...', statuses, '\n')
+    console.log('GTA PC:', statuses.gtao.PC)
+
+    Object.entries(statuses).forEach(([key, value]) => {
+        console.log(`\n${key}:`)
+
+        if (key != 'lastUpdate') {
+            Object.entries(value).forEach(([key, value]) => {
+                console.log(`\t${key}: ${value}`)
+            })
+        } else {
+            console.log('\t', value)
         }
-
-        try {
-            const object = JSON.parse(fileData)
-            return cb && cb(null, object)
-
-        } catch (err) {
-            return cb && cb(err)
-        }
-    })
-}
-
-jsonReader("./LogRockstarsServices.json", (err, customer) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-
-    fetchStatus().then((statuses) => {
-        writeFile("./LogRockstarsServices.json", JSON.stringify(statuses, null, 4), (error) => {
-            if (error) {
-                console.log('An error has occurred ', error)
-                return
-            }
-
-            console.log('Data written successfully to disk')
-        })
     })
 })
-
